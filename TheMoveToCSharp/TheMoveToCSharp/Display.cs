b6a543cdf1b8d475ad79f61365a7e7f9d6873cf0
David Basarab <dbasarab617@gmail.com>
2015-05-05 10:16:37 -0400

    Started C# project.

---
+++ TheMoveToCSharp/TheMoveToCSharp/Display.cs
@@ -0,0 +1,30 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+
+
+namespace TheMoveToCSharp
+{
+    public class Display
+    {
+        private readonly GameWindow _gameWindow;
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        public Display(int width, int height)
+        {
+            Width = width;
+            Height = height;
+
+            _gameWindow = new GameWindow(Width, Height, GraphicsMode.Default);
+
+            _gameWindow.Title = "Moving to CSharp Window";
+
+            
+        }
+
+        public void Run()
+        {
+            _gameWindow.Run(60, 60);
+        }
+    }
+}
---
+++ TheMoveToCSharp/TheMoveToCSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TheMoveToCSharp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            StartGameLoop();
+
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey(false);
+        }
+
+        private static void RunSample()
+        {
+            var display = new Display(800, 600);
+
+            display.Run();
+        }
+
+        private static void StartGameLoop()
+        {
+            Task.Run(() => StartOpenGl());
+        }
+
+        private static void StartOpenGl()
+        {
+            try
+            {
+                RunSample();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:      {0}", ex.Message);
+                Console.WriteLine("StackTrack: {0}", ex.StackTrace);
+            }
+        }
+    }
+}
