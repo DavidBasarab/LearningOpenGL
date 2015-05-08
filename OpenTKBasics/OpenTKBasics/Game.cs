d4484647b06fc3b506c16ac0bd87b82a611ed20d
David Basarab <dbasarab617@gmail.com>
2015-05-08 09:58:27 -0400

    Got it working drawing from Vertex object.

--- OpenTKBasics/OpenTKBasics/Game.cs
+++ OpenTKBasics/OpenTKBasics/Game.cs
@@ -8,7 +8,7 @@ namespace OpenTKBasics
 {
     public class Game
     {
-        private Vector2[] _vertexBuffer;
+        private Vertex[] _vertexBuffer;
         private int _vertexBufferObject;
         public Texture2D Texture2D { get; set; }
         public GameWindow Window { get; set; }
@@ -18,46 +18,6 @@ namespace OpenTKBasics
             Window = gameWindow;
 
             RegisterForEvents();
-        }
-
-        private void OnClosing(object sender, CancelEventArgs e) {}
-
-        private void OnRenderFrame(object sender, FrameEventArgs e)
-        {
-            //GL.ClearColor(Color4.DarkBlue);
-            GL.ClearColor(Color.FromArgb(5, 5, 25));
-            GL.Clear(ClearBufferMask.ColorBufferBit);
-
-            GL.Enable(EnableCap.Blend);
-            GL.Enable(EnableCap.Texture2D);
-            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
-
-            var world = Matrix4.CreateOrthographicOffCenter(0, Window.Width, Window.Height, 0, 0, 1);
-            GL.MatrixMode(MatrixMode.Modelview);
-
-            GL.LoadMatrix(ref world);
-
-            DrawOldSchoolVertexes();
-
-            DrawVertexBuffers();
-
-            GL.Flush();
-
-            Window.SwapBuffers();
-        }
-
-        private void DrawVertexBuffers()
-        {
-            GL.EnableClientState(ArrayCap.VertexArray);
-            GL.EnableClientState(ArrayCap.TextureCoordArray);
-
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-
-            GL.VertexPointer(2, VertexPointerType.Float, Vector2.SizeInBytes * 2, 0);
-            GL.TexCoordPointer(2, TexCoordPointerType.Float, Vector2.SizeInBytes * 2, Vector2.SizeInBytes);
-
-            GL.Color3(Color.BlueViolet);
-            GL.DrawArrays(PrimitiveType.Quads, 0, _vertexBuffer.Length / 2);
         }
 
         private static void DrawOldSchoolVertexes()
@@ -83,6 +43,51 @@ namespace OpenTKBasics
             GL.End();
         }
 
+        private void DrawVertexBuffers()
+        {
+            GL.EnableClientState(ArrayCap.ColorArray);
+            GL.EnableClientState(ArrayCap.VertexArray);
+            GL.EnableClientState(ArrayCap.TextureCoordArray);
+            
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+
+            // The order of the pointers matter on how the struct is order is right now color, position, texCoord
+
+            GL.ColorPointer(4, ColorPointerType.Float, Vertex.SizeInBytes, (IntPtr)(0)); // Step over Position and Texture
+            GL.VertexPointer(2, VertexPointerType.Float, Vertex.SizeInBytes, (IntPtr)(Vector4.SizeInBytes));
+            GL.TexCoordPointer(2, TexCoordPointerType.Float, Vertex.SizeInBytes, (IntPtr)(Vector4.SizeInBytes + Vector2.SizeInBytes));
+            
+
+            //GL.Color3(Color.BlueViolet);
+            GL.DrawArrays(PrimitiveType.Quads, 0, _vertexBuffer.Length);
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e) {}
+
+        private void OnRenderFrame(object sender, FrameEventArgs e)
+        {
+            //GL.ClearColor(Color4.DarkBlue);
+            GL.ClearColor(Color.FromArgb(5, 5, 25));
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Enable(EnableCap.Blend);
+            GL.Enable(EnableCap.Texture2D);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+
+            var world = Matrix4.CreateOrthographicOffCenter(0, Window.Width, Window.Height, 0, 0, 1);
+            GL.MatrixMode(MatrixMode.Modelview);
+
+            GL.LoadMatrix(ref world);
+
+            //DrawOldSchoolVertexes();
+
+            DrawVertexBuffers();
+
+            GL.Flush();
+
+            Window.SwapBuffers();
+        }
+
         private void OnUpdateFrame(object sender, FrameEventArgs e)
         {
             // e.Time Time in Seconds since last update
@@ -94,17 +99,22 @@ namespace OpenTKBasics
 
             _vertexBuffer = new[]
                             {
-                                    new Vector2(0, 100), new Vector2(0, 0),
-                                    new Vector2(100, 100), new Vector2(1, 0),
-                                    new Vector2(100, 200), new Vector2(1, 1),
-                                    new Vector2(0, 200), new Vector2(0, 1)
+                                    new Vertex(new Vector2(0, 0), new Vector2(0, 0)),
+                                    new Vertex(new Vector2(300, 0), new Vector2(1, 0)),
+                                    new Vertex(new Vector2(300, 300), new Vector2(1, 1))
+                                    {
+                                            Color = Color.Red
+                                    },
+                                    new Vertex(new Vector2(0, 300), new Vector2(0, 1)),
+                                    //new Vertex(new Vector2(200, 0), new Vector2(0, 0)),
+                                    //new Vertex(new Vector2(200, 100), new Vector2(0, 1))
                             };
 
             _vertexBufferObject = GL.GenBuffer();
 
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
 
-            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(Vector2.SizeInBytes * _vertexBuffer.Length), _vertexBuffer, BufferUsageHint.StaticDraw);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(Vertex.SizeInBytes * _vertexBuffer.Length), _vertexBuffer, BufferUsageHint.StaticDraw);
 
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
         }
---
+++ OpenTKBasics/OpenTKBasics/Vertex.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using OpenTK;
+
+namespace OpenTKBasics
+{
+    public struct Vertex
+    {
+        public Vector4 color;
+        public Vector2 position;
+        public Vector2 texCoord;
+
+        public Color Color
+        {
+            get { return Color.FromArgb((int)(255 * color.W), (int)(255 * color.X), (int)(255 * color.Y), (int)(255 * color.Z)); }
+            set { color = new Vector4(value.R / 255f, value.G / 255f, value.B / 255f, value.A / 255f); }
+        }
+
+        public static int SizeInBytes
+        {
+            get { return Marshal.SizeOf(typeof(Vertex)); }
+        }
+
+        public Vertex(Vector2 position, Vector2 texCoord)
+        {
+            this.position = position;
+            this.texCoord = texCoord;
+            color = new Vector4(1, 1, 1, 1);
+        }
+    }
+}
