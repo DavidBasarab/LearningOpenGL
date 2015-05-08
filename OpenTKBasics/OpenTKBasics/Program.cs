39543006e4c9a1e3f445421a0c3adc095cc82b78
David Basarab <dbasarab617@gmail.com>
2015-05-08 06:33:52 -0400

    Start OpenTKBasics

---
+++ OpenTKBasics/OpenTKBasics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OpenTKBasics
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit. . . . . .");
+
+            Console.ReadKey(false);
+        }
+    }
+}
