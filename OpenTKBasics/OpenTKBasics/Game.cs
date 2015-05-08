71af7f0c5e7bd6769abdc96e86a791fe48e6ebcc
David Basarab <dbasarab617@gmail.com>
2015-05-08 06:43:18 -0400

    Got started with basic window.

---
+++ OpenTKBasics/OpenTKBasics/Game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenTKBasics
+{
+    public class Game
+    {
+        public GameWindow Window { get; set; }
+
+        public Game(GameWindow gameWindow)
+        {
+            Window = gameWindow;
+
+            RegisterForEvents();
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e) {}
+
+        private void OnRenderFrame(object sender, FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.Flush();
+
+            Window.SwapBuffers();
+        }
+
+        private void OnUpdateFrame(object sender, FrameEventArgs e)
+        {
+            // e.Time Time in Seconds since last update
+        }
+
+        private void OnWindowLoad(object sender, EventArgs e)
+        {
+            GL.ClearColor(Color4.Coral);
+        }
+
+        private void RegisterForEvents()
+        {
+            Window.Load += OnWindowLoad;
+            Window.RenderFrame += OnRenderFrame;
+            Window.UpdateFrame += OnUpdateFrame;
+            Window.Closing += OnClosing;
+        }
+    }
+}
--- OpenTKBasics/OpenTKBasics/Program.cs
+++ OpenTKBasics/OpenTKBasics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OpenTK;
 
 namespace OpenTKBasics
 {
@@ -6,6 +7,12 @@ namespace OpenTKBasics
     {
         private static void Main(string[] args)
         {
+            var window = new GameWindow(800, 600);
+
+            var game = new Game(window);
+
+            window.Run();
+
             Console.WriteLine("Press any key to exit. . . . . .");
 
             Console.ReadKey(false);
